5cd0834f42560fb5c8ef3d9017aedac1749a48ec
guozhongping <guozhongping@aixuedai.com>
2017-03-31 13:49:36 +0800

    脚本修改

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-    $PROJECT_PATH=/root/my-code/
+    PROJECT_PATH=/root/my-code/
 	echo "Starting my-code ..."
 	nohup java -Xbootclasspath/a:`echo $PROJECT_PATH/lib/*.jar| tr ' ' ':'`: -jar $PROJECT_PATH/lib/my-code*.jar > /root/logs/stdout.log 2>&1 &
 	echo "Started my-code ..."
